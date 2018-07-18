9655eff644690440d2f0ac600bdc4e14c28c4e15
Bill <dek_keng@hotmail.com>
2018-07-18 15:41:56 +0700

    Revise some duplicate code

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -14,19 +14,18 @@ class DateController extends Controller
         // Only calculate when set date_start and date_end
         if(isset($_GET['date_start']) && isset($_GET['date_end'])) {
             $date_start = Carbon::createFromFormat('d/m/Y', $_GET['date_start']);
-            $date_end = Carbon::createFromFormat('d/m/Y', $_GET['date_end']);
+            $date_end = Carbon::createFromFormat('d/m/Y', $_GET['date_end'])->addDay(); // Add 1 day for inclusive
 
             // a) It should return the number of days between Start Date and End Date (both inclusive).
-            $result['a'] = $date_start->diffInDays($date_end)+1;
+            $result['a'] = $date_start->diffInDays($date_end);
 
             // b) It should return the number of days between Start Date and End Date (both inclusive) where the last day of the month fall on a Sunday.
             $result['b'] = 0; // Count days
             $result['sundays'] = []; // Store date string
             // Loop between date and get end of each month
-            // Get only end of month
+            // Get first end of month
             $b = $date_start->copy()->endOfMonth();
-            // Add another day for inclusive date end
-            while($b->lte($date_end->copy()->addDay())) {
+            while($b->lte($date_end)) {
                 // Check if it sunday
                 if($b->isSunday()) {
                     $result['sundays'][] = $b->format('d/m/Y');
